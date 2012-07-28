71dfb347080de73631b45e56090b024ec1a77d06
Michael <htp.michael@gmail.com>
2012-07-28 21:01:39 +0400

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BulletinBoard::Application.config.secret_token = 'b9e313a23aff6d19ebd9e2a6ebd61c526aa49b85c2596c2678a67137a82554e6e9d064c9246bb10041817eed4cfc9ac89d47ec658eab3e442186a23f001e76b4'
